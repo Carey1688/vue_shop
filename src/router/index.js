469c5576021f21d854e178cab978fd0107c9244b
kr <kangrui@jovision.com>
2020-09-01 17:08:15 +0800

    完成商品功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,9 @@ import Users from '../components/user/Users.vue'
 import Rights from '../components/right/Rights.vue'
 import Roles from '../components/right/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
+import List from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
 
 Vue.use(Router)
 
@@ -44,6 +47,18 @@ const router = new Router({
     {
       path: '/categories',
       component: Cate
+    },
+    {
+      path: '/params',
+      component: Params
+    },
+    {
+      path: '/goods',
+      component: List
+    },
+    {
+      path: '/goods/add',
+      component: Add
     }
     ]
   }
